7df1163485cc9fd8ef78e8298a11a4108660a6b8
janko33 <rakonjacj@gmail.com>
2023-11-28 02:18:09 +0100

    implementing delete front end logic

--- client/src/app/services/reservation-service/reservation.service.ts
+++ client/src/app/services/reservation-service/reservation.service.ts
@@ -5,17 +5,21 @@ import { apiURL } from 'src/app/domains/constants';
 import { ToastService } from '../toast/toast.service';
 import { Router } from '@angular/router';
 import { ToastNotificationType } from 'src/app/domains/enums/toast-notification-type.enum';
+import { UserService } from '../user/user.service';
+import { UserAuth } from 'src/app/domains/entity/user-auth.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
   private apiURL = apiURL;
+ 
 
   constructor(
     private http: HttpClient,
     private toastService: ToastService,
-    private router: Router
+    private router: Router,
+    private userService: UserService 
   ) {}
 
   createReservation(reservationData: any): Observable<any> {
@@ -37,6 +41,49 @@ export class ReservationService {
           throw err;
         })
       );
+  }
+
+  getLoggedUserId(): string | null {
+    const loggedUser = this.userService.getLoggedUser();
+  
+    if (loggedUser) {
+      return loggedUser.id; 
+    } else {
+    
+      return null;
+    }
+  }
+
+  deleteById(
+    id:string,
+    
+    
+  ): void {
+    this.http
+      .delete(`${apiURL}${this.getLoggedUserId}/reservations/${id}`, {
+       
+      })
+      .subscribe({
+        next: (data) => {
+          this.toastService.showToast(
+            'Success',
+            'Reservation deleted!',
+            ToastNotificationType.Success
+            
+          );
+          
+          
+          
+        },
+        error: (err) => {
+          this.toastService.showToast(
+            'Error',
+            err.error.error,
+            ToastNotificationType.Error
+            );
+          },
+        });
+        this.router.navigate(['/']);
   }
   
 }
