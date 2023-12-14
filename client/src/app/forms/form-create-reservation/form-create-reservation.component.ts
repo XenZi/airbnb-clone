34e32db83173e5d06c783dfcb803a1b2acabe44a
XenZi <rilakmateja@gmail.com>
2023-12-14 21:46:23 +0100

    feat: additional fields

--- client/src/app/forms/form-create-reservation/form-create-reservation.component.ts
+++ client/src/app/forms/form-create-reservation/form-create-reservation.component.ts
@@ -34,10 +34,20 @@ export class ReservationFormComponent implements OnInit {
 
   handleDateChange(rangeDates: Date[]) {
     console.log(rangeDates);
-    const formattedRange = rangeDates.map((date) => date.toUTCString());
+    const formattedRange = rangeDates.map((date) => this.formatDate(date));
     this.reservationForm.get('range')?.setValue(formattedRange);
   }
 
+  formatDate(date: Date) {
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let newDay = (day < 10 ? '0' + day : day) as string;
+    let newMonth = month < 10 ? '0' + month : month;
+
+    return `${year}-${newMonth}-${newDay}`;
+  }
   submitReservation() {
     if (!this.reservationForm.valid) {
       console.log('not valid');
@@ -48,11 +58,14 @@ export class ReservationFormComponent implements OnInit {
       this.reservationForm.value.range[
         this.reservationForm.value.range.length - 1
       ];
+    console.log(this.accommodation.location);
     let userID: string = this.user?.id as string;
     let username: string = this.user?.username as string;
     let accommodationName: string = this.accommodation.name;
     let location: string = this.accommodation.location;
     let price: number = 50;
+    let numOfDays: number = this.reservationForm.value.range.length;
+    let dateRange: string[] = this.reservationForm.value.range;
     console.log(
       startDate,
       endDate,
@@ -60,7 +73,9 @@ export class ReservationFormComponent implements OnInit {
       username,
       accommodationName,
       location,
-      price
+      price,
+      numOfDays,
+      dateRange
     );
   }
 }
